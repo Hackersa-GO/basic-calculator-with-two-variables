5c99ed6a54e26475a3976a8cb5e679df351abf0f
Antonio-J <joaoantoniomatos2008@icloud.com>
2025-10-19 11:18:21 -0300

    Add files via upload

---
+++ Cal_basic.java
@@ -0,0 +1,16 @@
+public class Cal_basic {
+public static void main(String[] args) {
+    //é preciso fazer o uso de números inteiros//
+
+    int a=10;
+    int b=5;
+
+    //agora é preciso fazer a soma,subtração,divisão e multiplicação dos valores citados//
+
+    System.out.println("a soma é "+(a+b));
+    System.out.println("a subtração é "+(a-b));
+    System.out.println("a multiplicação é "+(a*b));
+    System.out.println("a divisão é "+(a/b));
+
+}
+}
